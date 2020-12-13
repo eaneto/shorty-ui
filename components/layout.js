1efdc7f62b9cdc666060139395d33e4ed70ca3ce
Edison Neto <ednetoali@gmail.com>
2020-12-13 18:38:40 -0300

    Add initial form

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,12 @@
+import Head from 'next/head'
+import homeStyle from '../styles/Home.module.css'
+
 export default function Layout({ children, home }) {
     return (
-        <div>
+        <div className={homeStyle.container}>
+            <Head>
+                <title>Shorty - URL Shortener</title>
+            </Head>
             <h1>Shorty</h1>
             <main>
                 {children}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,34 @@
-import Head from 'next/head'
+import Layout from '../components/layout'
+import homeStyle from '../styles/Home.module.css'
+import React from 'react'
 
 export default function Home() {
   return (
-    <>
-    <Head>
-      <title>Shorty - URL Shortener</title>
-    </Head>
-    <h1>Shorty</h1>
-    </>
+    <Layout>
+      <form className={homeStyle.form} onSubmit={handleShortenRequest}>
+        <h2>Shorten URL</h2>
+        <input type="text" className={homeStyle.form_input} name="original-url" placeholder="URL" />
+        <input type="submit" className={homeStyle.submit_button} value="Shorten" />
+      </form>
+
+      <form className={homeStyle.form} onSubmit={handleShortenRequest}>
+        <h2>Unshorten URL</h2>
+        <input type="text" className={homeStyle.form_input} name="shortened-url" placeholder="Shortened URL" />
+        <input type="submit" className={homeStyle.submit_button} value="Unshorten" />
+      </form>
+    </Layout>
   )
 }
+
+async function handleShortenRequest(event) {
+  console.log(event)
+  /* const response = await fetch("http://eaneto-shorty.herokuapp.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+  const data = await response.json()*/
+}
+
